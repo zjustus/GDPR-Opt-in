a57795118ba8dcae993b0add2f224782c663dee5
Zach Justus <zach.justus1@gmail.com>
2023-11-03 15:00:46 -0700

    Finished base project

--- gdpr-processor_template.js
+++ gdpr-processor_template.js
@@ -1,10 +1,20 @@
+const setDefaultConsentState = require('setDefaultConsentState');
 const updateConsentState = require('updateConsentState');
 const callInWindow = require('callInWindow');
 
+setDefaultConsentState({
+	'ad_storage': 'denied',
+	'analytics_storage': 'granted',
+	'functionality_storage': 'granted',
+	'personalization_storage': 'granted',
+	'security_storage': 'granted',
+	'wait_for_update': 500
+});
+
 const consentChange = (consentObjecgt) => {
-	log("Consent has been updated");
 	updateConsentState(consentObjecgt);
 };
 
 callInWindow('consentUpdate', consentChange);
+callInWindow('showConsentBanner');
 data.gtmOnSuccess();
